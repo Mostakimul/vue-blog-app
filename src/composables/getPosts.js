78548fb8b2d410f6144e5a58354b0eb8442450f9
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-08-20 11:58:33 +0200

    firebase realtime blog fetch

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -8,6 +8,17 @@ const getPosts = () => {
   // fetching data
   const load = async () => {
     try {
+      // For realtime update from firebase
+      const response = await vueBlogFirestore
+        .collection('posts')
+        .orderBy('created_at', 'desc')
+        .onSnapshot((snap) => {
+          posts.value = snap.docs.map((doc) => {
+            return { ...doc.data(), id: doc.id };
+          });
+        });
+
+      /*
       // connection to firebase database
       const response = await vueBlogFirestore
         .collection('posts')
@@ -19,6 +30,7 @@ const getPosts = () => {
         // spreading the data will get all different propertise
         return { ...doc.data(), id: doc.id };
       });
+      */
     } catch (err) {
       error.value = err.message;
       // console.log(error.value);
