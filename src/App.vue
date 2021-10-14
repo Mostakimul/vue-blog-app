6cbb59e1c511056762fd66b8f2e8d0a002e15493
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-15 00:32:01 +0200

    responsive class changed to main app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
+<div class="px-4 sm:px-6 md:px-8 max-w-screen-lg xl:max-w-screen-xl mx-auto">
   <TheHeader />
   <router-view />
+</div>
 </template>
 <script>
 import TheHeader from '@/components/TheHeader.vue';
